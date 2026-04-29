0440e34030dbc809aa60abbc9557571df48d92cc
Felipe Sepulvida Engel <sepulvida.eng@gmail.com>
2026-04-29 20:55:14 -0300

    feat(engine): expor utilitários de pontuação e configuração da trilha do Linguista

--- src/lib/engine/index.ts
+++ src/lib/engine/index.ts
@@ -1,3 +1,9 @@
 export { createInitialGame, startNewSeason } from "./setup";
 export { applyAction } from "./turns";
 export { validateExpedition } from "./validation";
+export {
+  pointsForExpeditionSize,
+  EXPEDITION_POINTS,
+  LINGUIST_TRACK_LENGTH,
+  LINGUIST_ARTIFACT_SPACES,
+} from "./config";
